86ee6cead680c17ba6db148ab42c849b886eb7f3
marcosd59 <damian.marcospool@gmail.com>
2024-01-27 17:38:50 -0500

    Refactor Testimonio component to use props

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,25 +1,18 @@
 import React from "react";
-import "../stylesheets/Testimonio.css" ;
+import "../stylesheets/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
-      <img
-        className="imagen-testimonio"
-        src={require("../img/testimonio-emma.png")}
-        alt="Foto de Emma"
-      />
+      <img className="imagen-testimonio" src={require(`../img/testimonio-${props.imagen}.png`)} alt="Foto de Emma" />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera en Software en Spotify</p>
-        <p className="texto-testimonio">
-          "Siempre he tenido problemas para aprender JavaScript. He tomado
-          muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
-          Estudiar JavaScript, así como estructuras de datos y algoritmos en
-          freeCodeCamp me dio las habilidades y la confianza que necesitaba para
-          conseguir el trabajo de mis sueños como ingeniero de software en
-          Spotify."
+        <p className="nombre-testimonio">
+          {props.nombre} en {props.pais}
         </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en {props.empresa}
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
