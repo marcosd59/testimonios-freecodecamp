064253b4b0d73f4be8fd507bed87bb5c01d0af18
marcosd59 <damian.marcospool@gmail.com>
2024-01-27 17:44:54 -0500

    Refactor Testimonio component

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -4,13 +4,17 @@ import "../stylesheets/Testimonio.css";
 function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require(`../img/testimonio-${props.imagen}.png`)} alt="Foto de Emma" />
+      <img
+        className="imagen-testimonio"
+        src={require(`../img/testimonio-${props.imagen}.png`)}
+        alt="Foto de Emma"
+      />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
-          {props.nombre} en {props.pais}
+          <strong>{props.nombre}</strong> en {props.pais}
         </p>
         <p className="cargo-testimonio">
-          {props.cargo} en {props.empresa}
+          {props.cargo} en <strong>{props.empresa}</strong>
         </p>
         <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
