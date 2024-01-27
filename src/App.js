40e07b95211374e0bd7392414be8dadcdc5b6c39
marcosd59 <damian.marcospool@gmail.com>
2024-01-27 17:43:07 -0500

    Add testimonial from Shawn Wang

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
       <div>
         <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp:</h1>
         <Testimonio
+          nombre="Shawn Wang"
+          pais="Singapur"
+          imagen="shawn"
+          cargo="Ingeniero de Software"
+          empresa="Amazon"
+          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+        />
+        <Testimonio
           nombre="Sarah Chima"
           pais="Nigeria"
           imagen="sarah"
